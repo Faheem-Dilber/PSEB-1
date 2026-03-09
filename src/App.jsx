143e3e59d3dd98b254f76cb198f0e5c1c87f8bb3
Faheem-Dilber <faheem849136@gmail.com>
2026-03-09 16:13:23 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Button } from "./components/ui/button.jsx";
+function App() {
+  return (
+    <div>
+      <Button variant="outline">Hello World</Button>
+    </div>
+  );
+}
+
+export default App;
